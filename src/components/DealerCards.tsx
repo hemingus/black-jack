fcdd7ad65d4649a194af16e3d25daeb865f70d09
hemingus <heming.hanevik@gmail.com>
2025-11-11 15:20:33 +0100

    implement DealerCards showdown handling

--- src/components/DealerCards.tsx
+++ src/components/DealerCards.tsx
@@ -1,15 +1,25 @@
 import { type Card } from "../types"
+import { cardBackImageURL } from "../api/DeckOfCards"
 
 interface DealerCardsProps {
     cards: Array<Card>
+    showDown: boolean
 }
 
-export default function DealerCards({ cards }: DealerCardsProps ) {
+export default function DealerCards({ cards, showDown }: DealerCardsProps ) {
     return (
         <div>
             <h3>Dealer Cards</h3>
             <div>
-                {cards.map((card, index) => <img style={{width: "100px"}} key={card.code + index} src={card.image}/>)}
+                {showDown ? 
+                cards.map((card, index) => 
+                    <img style={{width: "100px"}} key={card.code + index} src={card.image}/>)
+                :
+                cards.map((card, index) => {
+                    if (index === 1) return <img key={card.code + index}style={{width: "100px"}} src={cardBackImageURL}/>
+                    else return <img key={card.code + index} style={{width: "100px"}} src={card.image}/>
+                })
+                }
             </div>
         </div>
     )
